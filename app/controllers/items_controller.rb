be7fa669002de8da20980135ee13fb5ff1a92a20
MMcClure11 <mcclme02@luther.edu>
2020-09-24 16:46:07 -0500

    adds create action and item_params methods to items_controller, and category_name getter and setter in Item model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,14 @@ class ItemsController < ApplicationController
     render json: ItemSerializer.new(items).to_serialized_json
   end
 
+  def create
+    item = Item.new(item_params)
+    item.save
+    render json: ItemSerializer.new(item).to_serialized_json
+  end
+
+  def item_params
+    params.permit(:name, :category_name)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :category
+
+  def category_name=(name)
+    category = Category.find_by(name: name)
+    self.category = category
+  end
+
+  def category_name
+    category.name
+  end
 end
