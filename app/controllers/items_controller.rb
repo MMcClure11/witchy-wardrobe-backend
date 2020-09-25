130192976377d0b4aa7cab3ce192229dcdf36693
MMcClure11 <mcclme02@luther.edu>
2020-09-25 09:08:03 -0500

    adds destroy method to item contorller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,15 @@ class ItemsController < ApplicationController
     render json: ItemSerializer.new(item).to_serialized_json
   end
 
+  def destroy
+    item = Item.find_by(id: params[:id])
+    if item.destroy 
+      render json: {message: "Successfully deleted"}
+    end
+  end
+
+  private
+
   def item_params
     params.permit(:name, :category_name)
   end
