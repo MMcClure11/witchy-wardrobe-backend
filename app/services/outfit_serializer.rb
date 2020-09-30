c43c010faf2a12aed3041270f7867d48f6c5a933
MMcClure11 <mcclme02@luther.edu>
2020-09-30 13:01:28 -0500

    adds initialize and to_serialized_json methods to OUtfitSerializer class

---
+++ app/services/outfit_serializer.rb
@@ -0,0 +1,14 @@
+class OutfitSerializer
+
+  def initialize(outfit_object)
+    @outfit = outfit_object
+  end
+
+  def to_serialized_json
+    @outfit.to_json(:except => [:created_at, :updated_at],
+    :include => {
+      :items => {:only => [:name, :image, :id]}
+    })
+  end
+  
+end
