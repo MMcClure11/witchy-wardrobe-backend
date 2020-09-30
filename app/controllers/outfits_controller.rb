90c64af6930bed5468fb017dbe849b1cb5f3bd57
MMcClure11 <mcclme02@luther.edu>
2020-09-30 18:36:31 -0500

    adds create method and outfit params to outfit controller

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -3,7 +3,20 @@ class OutfitsController < ApplicationController
   def index
     outfits = Outfit.all
     render json: OutfitSerializer.new(outfits).to_serialized_json
-  
   end
 
+  def create 
+    outfit = Outfit.new(outfit_params)
+    if outfit.save
+      render json: OutfitSerializer.new(outfit).to_serialized_json
+    else
+      render json: {errors: outfit.errors.full_messages.to_sentence}, status: :unprocessable_entity
+    end
+  end 
+
+  private 
+
+  def outfit_params
+    params.permit(:name, :likes, :item_ids => [])
+  end 
 end
