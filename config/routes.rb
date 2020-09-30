1283d7934340120cf8bb31b927afb2fe49cba460
MMcClure11 <mcclme02@luther.edu>
2020-09-30 10:25:19 -0500

    cleans up routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :categories
+  resources :items, only: [:index, :create, :update, :destroy]
+  resources :categories, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
