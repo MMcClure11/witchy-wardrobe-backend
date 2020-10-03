985de3e12973e2533aa4c2ee6e3d5830a3d7cf5f
MMcClure11 <mcclme02@luther.edu>
2020-10-03 11:04:34 -0500

    adds color to outfit serializer for items

--- app/services/outfit_serializer.rb
+++ app/services/outfit_serializer.rb
@@ -7,7 +7,7 @@ class OutfitSerializer
   def to_serialized_json
     @outfit.to_json(:except => [:created_at, :updated_at],
     :include => {
-      :items => {:only => [:name, :image, :id]}
+      :items => {:only => [:name, :image, :id, :color]}
     })
   end
   
